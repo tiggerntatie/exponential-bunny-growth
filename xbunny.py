0d7a2dae858e062ca02997482347cf5974b1cbf6
Eric Dennison <ericd@netdenizen.com>
2022-01-28 12:21:50 -0500

    Updated from Brython Server: 1/28/2022 12:21:50 PM

--- xbunny.py
+++ xbunny.py
@@ -1,62 +1,47 @@
-from ggame import App, ImageAsset, Sprite, MouseEvent
+from ggame import ImageAsset, Sprite, MouseEvent
 from random import random, randint
+from ggame.timer import Timer
+from ggame.mathapp import MathApp
+
+
+bunnylist = []
+
+def timercallback(t):
+    """
+    Callback function to receive notification of timer expiration.
+    """
+    print("time's up at", t.time, "seconds!")
+
 
 class Bunny(Sprite):
 
     asset = ImageAsset("bunny.png")
 
+    @classmethod
+    def randomBunny(cls, width, height):
+        return cls((randint(50,width),randint(50,height)))
+
     def __init__(self, position):
         super().__init__(Bunny.asset, position)
-        # register mouse events
-        App.listenMouseEvent(MouseEvent.mousedown, self.mousedown)
-        App.listenMouseEvent(MouseEvent.mouseup, self.mouseup)
-        App.listenMouseEvent(MouseEvent.mousemove, self.mousemove)
-        self.dragging = False
 
 
     def step(self):
-        """
-        Every now and then a bunny hops...
-        """
-        if random() < 0.01:
-            self.x += randint(-20,20)
-            self.y += randint(-20,20)
+        pass
+    
+    
 
 
-    def mousedown(self, event):
-        # capture any mouse down within 50 pixels
-        self.deltax = event.x - (self.x + self.width//2)
-        self.deltay = event.y - (self.y + self.height//2)
-        if abs(self.deltax) < 50 and abs(self.deltay) < 50:
-            self.dragging = True
-            # only drag one bunny at a time - consume the event
-            event.consumed = True
 
-    def mousemove(self, event):
-        if self.dragging:
-            self.x = event.x - self.deltax - self.width//2
-            self.y = event.y - self.deltay - self.height//2
-            event.consumed = True
-
-    def mouseup(self, event):
-        if self.dragging:
-            self.dragging = False
-            event.consumed = True
-
-
-class DemoApp(App):
+class DemoApp(MathApp):
 
     def __init__(self):
         super().__init__()
-        for i in range(10):
-            Bunny((randint(50,self.width),randint(50,self.height)))
-
-    def step(self):
-        """
-        Override step to perform action on each frame update
-        """
-        for bunny in self.spritelist:
-            bunny.step()
+        for i in range(2):
+            bunnylist.append(Bunny.randomBunny(self.width, self.height))
+        self.TIMER = Timer()
+        # Execute timercallback after 1 seconds
+        self.TIMER.callAfter(1, timercallback)
+        
 
 
 # Create the app
